5c9aea9e51e1a72345a72751005487f30b96cffe
healgon369star <114792895+healgon369star@users.noreply.github.com>
2022-10-08 15:04:03 -0500

    Add files via upload

---
+++ TRABAJO COURSERA.py
@@ -0,0 +1,54 @@
+<h1>MY JUPYTER NOTEBOOK ON IBM WATSON STUDIO</h1>
+
+<strong>PAUL STEVEN VELASQUEZ VEGA</strong>
+# yo soy inevitable
+# y Yo 
+# Yo soy !Iron Man¡
+
+<i>Mi interés en la ciencia de datos es importe para mi trabajo en la parte comercial y contable de las empresas </i>
+
+<h3>Valor a cobrar de una tienda por la venta de unas sapatillas zapatillas Nike Jordan 1,"factura"</h3>
+
+In[5]:
+# Valor sin descuento por unidad de unas sapatillas zapatillas Nike Jordan 1 
+valor_nkj1 = 625
+# Valor en porcentaje de descuento en pago efectivo de la tienda 
+descuento = 0.2 
+# Valor de la cantidad de unidades a vender
+cantidad_nkj1 = 1
+
+# Valor del descuento por la venta
+valor_descuento = (cantidad_nkj1 * valor_nkj1) * descuento
+# Valor neto a cobrar
+valor_pagar = ( valor_nkj1-valor_descuento)
+
+# valor_nkj1
+print("valor de Nike Jordan 1 en USD")
+print(valor_nkj1)
+# valor_descuento
+print("valor de descuento en USD")
+print(-valor_descuento)
+# valor_pagar
+print("valor a pagar en USD")
+print(valor_pagar)
+
+<ul> 
+    <li>Dunk Low</li>
+    <li>Overbreak</li>
+    <li>AF 1:</li>
+    <li>AJ 1</li>
+    <li>SACAI</li>
+</ul>
+
+<ol> 
+    <li>Dunk Ambush</li>
+    <li>AJ 4</li>
+    <li>MMW:</li>
+    <li>AM 90</li>
+    <li>air max 97</li>
+</ol>
+
+<a href="url">https://www.youtube.com/watch?v=s-hzc1JioQk</a>
+
+In[ ]:
+    
